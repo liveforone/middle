95d96d5c8c0fdb8ebe508cd72fddee9c6c46b89e
liveforone <yc4852@gmail.com>
2023-04-18 18:13:32 +0900

    Initial user-service.

---
+++ user-service/user-service/src/main/java/middle/userservice/UserServiceApplication.java
@@ -0,0 +1,21 @@
+package middle.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
+	}
+}
