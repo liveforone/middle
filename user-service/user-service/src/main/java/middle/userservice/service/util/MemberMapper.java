fbc0cc26fc40420246aa7723466f7416714390e5
liveforone <yc4852@gmail.com>
2023-04-19 20:16:11 +0900

    Add member null check in mapper.

--- user-service/user-service/src/main/java/middle/userservice/service/util/MemberMapper.java
+++ user-service/user-service/src/main/java/middle/userservice/service/util/MemberMapper.java
@@ -2,13 +2,19 @@ package middle.userservice.service.util;
 
 import middle.userservice.domain.Member;
 import middle.userservice.dto.response.MemberResponse;
+import middle.userservice.utility.CommonUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class MemberMapper {
 
     public static MemberResponse entityToDto(Member member) {
+        if (CommonUtils.isNull(member)) {
+            return new MemberResponse();
+        }
+
         return MemberResponse.builder()
                 .id(member.getId())
                 .email(member.getEmail())
@@ -18,6 +24,10 @@ public class MemberMapper {
     }
 
     public static List<MemberResponse> entityToDtoList(List<Member> members) {
+        if (CommonUtils.isNull(members)) {
+            return new ArrayList<>();
+        }
+
         return members
                 .stream()
                 .map(MemberMapper::entityToDto)
