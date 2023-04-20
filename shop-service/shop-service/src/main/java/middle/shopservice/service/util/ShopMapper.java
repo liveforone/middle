7bb258c386d4e8c8c3a1b7c582674db1953d7870
liveforone <yc4852@gmail.com>
2023-04-20 12:12:36 +0900

    Add entity to dto detail mapper.

--- shop-service/shop-service/src/main/java/middle/shopservice/service/util/ShopMapper.java
+++ shop-service/shop-service/src/main/java/middle/shopservice/service/util/ShopMapper.java
@@ -1,4 +1,25 @@
 package middle.shopservice.service.util;
 
+import middle.shopservice.domain.Shop;
+import middle.shopservice.dto.ShopResponse;
+import middle.shopservice.utility.CommonUtils;
+
 public class ShopMapper {
+
+    public static ShopResponse entityToDtoDetail(Shop shop) {
+        if (CommonUtils.isNull(shop)) {
+            return new ShopResponse();
+        }
+
+        return ShopResponse.builder()
+                .id(shop.getId())
+                .shopName(shop.getShopName())
+                .tel(shop.getTel())
+                .city(shop.getCity())
+                .street(shop.getStreet())
+                .detail(shop.getDetail())
+                .good(shop.getGood())
+                .bad(shop.getBad())
+                .build();
+    }
 }
