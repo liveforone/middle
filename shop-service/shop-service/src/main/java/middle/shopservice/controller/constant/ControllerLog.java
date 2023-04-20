74d657b520cb2106b50294b5744df2c06cbdd3a0
liveforone <yc4852@gmail.com>
2023-04-20 18:03:52 +0900

    Add update tel success controller log.

--- shop-service/shop-service/src/main/java/middle/shopservice/controller/constant/ControllerLog.java
+++ shop-service/shop-service/src/main/java/middle/shopservice/controller/constant/ControllerLog.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum ControllerLog {
 
     CREATE_SHOP_SUCCESS("상점을 성공적으로 등록했습니다."),
-    UPDATE_NAME_SUCCESS("상호를 성공적으로 수정했습니다. | Username : ");
+    UPDATE_NAME_SUCCESS("상호를 성공적으로 수정했습니다. | Username : "),
+    UPDATE_TEL_SUCCESS("전화번호를 성공적으로 수정했습니다. | Username : ");
 
     private final String value;
 }
