76128260bfd8f6890c9300d7899b967ec3bd3991
liveforone <yc4852@gmail.com>
2023-04-20 14:10:22 +0900

    Add auth check validator.

--- shop-service/shop-service/src/main/java/middle/shopservice/validator/ShopValidator.java
+++ shop-service/shop-service/src/main/java/middle/shopservice/validator/ShopValidator.java
@@ -1,9 +1,12 @@
 package middle.shopservice.validator;
 
 import lombok.RequiredArgsConstructor;
+import middle.shopservice.authentication.Role;
 import middle.shopservice.repository.ShopRepository;
 import middle.shopservice.utility.CommonUtils;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
@@ -15,5 +18,9 @@ public class ShopValidator {
         Long foundId = shopRepository.findOneIdForValidation(shopId);
 
         return CommonUtils.isNull(foundId);
+    }
+
+    public boolean isNotOwner(String auth) {
+        return !Objects.equals(auth, Role.OWNER.getValue());
     }
 }
