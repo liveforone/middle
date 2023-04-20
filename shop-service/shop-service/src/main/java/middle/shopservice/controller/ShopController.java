f08897e1306b39f0f7dcc407a01b542aa6e47b10
liveforone <yc4852@gmail.com>
2023-04-20 14:05:53 +0900

    Add shop detail controller method.

--- shop-service/shop-service/src/main/java/middle/shopservice/controller/ShopController.java
+++ shop-service/shop-service/src/main/java/middle/shopservice/controller/ShopController.java
@@ -2,7 +2,15 @@ package middle.shopservice.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import middle.shopservice.controller.constant.ParamConstant;
+import middle.shopservice.controller.constant.ShopUrl;
+import middle.shopservice.controller.restResponse.RestResponse;
+import middle.shopservice.dto.ShopResponse;
 import middle.shopservice.service.ShopService;
+import middle.shopservice.validator.ShopValidator;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class ShopController {
 
     private final ShopService shopService;
+    private final ShopValidator shopValidator;
+
+    @GetMapping(ShopUrl.SHOP_DETAIL)
+    public ResponseEntity<?> shopDetail(
+            @PathVariable(ParamConstant.SHOP_ID) Long shopId
+    ) {
+        if (shopValidator.isNull(shopId)) {
+            return RestResponse.shopIsNull();
+        }
+
+        ShopResponse shop = shopService.getShopById(shopId);
+        return ResponseEntity.ok(shop);
+    }
+    
+    //상정 생성시 auth owner인지 판별
 }
