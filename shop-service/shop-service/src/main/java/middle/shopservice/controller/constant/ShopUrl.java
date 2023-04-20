3e093fef4ddea6f2682c55de6acf8c7d82dc16c4
liveforone <yc4852@gmail.com>
2023-04-20 16:59:14 +0900

    Add home page url.

--- shop-service/shop-service/src/main/java/middle/shopservice/controller/constant/ShopUrl.java
+++ shop-service/shop-service/src/main/java/middle/shopservice/controller/constant/ShopUrl.java
@@ -4,4 +4,5 @@ public class ShopUrl {
 
     public static final String SHOP_DETAIL = "/shop/{shopId}";
     public static final String MY_SHOP = "/my-shop";
+    public static final String HOME_PAGE = "/shop/home";
 }
