4ea5008f5ec69744f2159dcf34f9878a5e519f19
liveforone <yc4852@gmail.com>
2023-04-20 18:13:12 +0900

    Add builder in shop request for comfortable unit test.

--- shop-service/shop-service/src/main/java/middle/shopservice/dto/ShopRequest.java
+++ shop-service/shop-service/src/main/java/middle/shopservice/dto/ShopRequest.java
@@ -1,11 +1,15 @@
 package middle.shopservice.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class ShopRequest {
 
     private Long id;
