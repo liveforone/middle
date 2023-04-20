79a27badf1bf130a8b9784a806a40e9884b41f24
liveforone <yc4852@gmail.com>
2023-04-20 18:09:50 +0900

    Add update address success controller log.

--- shop-service/shop-service/src/main/java/middle/shopservice/controller/constant/ControllerLog.java
+++ shop-service/shop-service/src/main/java/middle/shopservice/controller/constant/ControllerLog.java
@@ -9,7 +9,8 @@ public enum ControllerLog {
 
     CREATE_SHOP_SUCCESS("상점을 성공적으로 등록했습니다."),
     UPDATE_NAME_SUCCESS("상호를 성공적으로 수정했습니다. | Username : "),
-    UPDATE_TEL_SUCCESS("전화번호를 성공적으로 수정했습니다. | Username : ");
+    UPDATE_TEL_SUCCESS("전화번호를 성공적으로 수정했습니다. | Username : "),
+    UPDATE_ADDRESS_SUCCESS("주소를 성공적으로 수정했습니다. | Username : ");
 
     private final String value;
 }
