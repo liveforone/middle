53982fc59c340421c5bce72ba1b5f22d79ea7ffc
liveforone <yc4852@gmail.com>
2023-04-20 10:44:05 +0900

    Add nullable false option in username column.

--- shop-service/shop-service/src/main/java/middle/shopservice/domain/Shop.java
+++ shop-service/shop-service/src/main/java/middle/shopservice/domain/Shop.java
@@ -17,7 +17,7 @@ public class Shop {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = true, updatable = false)
+    @Column(unique = true, nullable = false, updatable = false)
     private String username;
 
     @Column(nullable = false)
