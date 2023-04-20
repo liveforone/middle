f85fcb808cb1b00d9a8b929c48f7e74abe69f988
liveforone <yc4852@gmail.com>
2023-04-20 14:08:02 +0900

    Add my shop url.

--- shop-service/shop-service/src/main/java/middle/shopservice/controller/constant/ShopUrl.java
+++ shop-service/shop-service/src/main/java/middle/shopservice/controller/constant/ShopUrl.java
@@ -3,4 +3,5 @@ package middle.shopservice.controller.constant;
 public class ShopUrl {
 
     public static final String SHOP_DETAIL = "/shop/{shopId}";
+    public static final String MY_SHOP = "/my-shop";
 }
