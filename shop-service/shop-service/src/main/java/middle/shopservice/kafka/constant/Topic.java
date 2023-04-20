81bc61be33509619772d9926be1546ce564d704f
liveforone <yc4852@gmail.com>
2023-04-20 13:38:35 +0900

    Add time table topic.

--- shop-service/shop-service/src/main/java/middle/shopservice/kafka/constant/Topic.java
+++ shop-service/shop-service/src/main/java/middle/shopservice/kafka/constant/Topic.java
@@ -5,5 +5,5 @@ public class Topic {
     public static final String SHOP_IS_GOOD = "shop-is-good";
     public static final String SHOP_IS_BAD = "shop-is-bad";
     public static final String REMOVE_SHOP_BELONG_MEMBER = "remove-shop-belong-member";
-    public static final String REMOVE_SHOP_RESERVATION = "remove-shop-reservation";
+    public static final String REMOVE_TIMETABLE = "remove-timetable";
 }
