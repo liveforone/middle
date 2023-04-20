34b351ede840597b54bf53c2fa90688b14c5244d
liveforone <yc4852@gmail.com>
2023-04-20 17:33:15 +0900

    Add city constant.

--- shop-service/shop-service/src/main/java/middle/shopservice/controller/constant/ParamConstant.java
+++ shop-service/shop-service/src/main/java/middle/shopservice/controller/constant/ParamConstant.java
@@ -6,4 +6,5 @@ public class ParamConstant {
     public static final String LAST_ID = "lastId";
     public static final String PAGE_SIZE = "pageSize";
     public static final String SHOP_NAME = "name";
+    public static final String CITY = "city";
 }
