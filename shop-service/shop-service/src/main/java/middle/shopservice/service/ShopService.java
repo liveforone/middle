66a22abbc678c1e8824deb0e645b0664c6942a4b
liveforone <yc4852@gmail.com>
2023-04-20 13:24:52 +0900

    Add get home page service method.

--- shop-service/shop-service/src/main/java/middle/shopservice/service/ShopService.java
+++ shop-service/shop-service/src/main/java/middle/shopservice/service/ShopService.java
@@ -2,10 +2,15 @@ package middle.shopservice.service;
 
 import lombok.RequiredArgsConstructor;
 import middle.shopservice.dto.ShopResponse;
+import middle.shopservice.feignClient.AdvertisementFeignService;
+import middle.shopservice.feignClient.constant.CircuitLog;
 import middle.shopservice.repository.ShopRepository;
 import middle.shopservice.service.util.ShopMapper;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,8 +18,24 @@ import org.springframework.transaction.annotation.Transactional;
 public class ShopService {
 
     private final ShopRepository shopRepository;
+    private final AdvertisementFeignService advertisementFeignService;
+    private final CircuitBreakerFactory<?, ?> circuitBreakerFactory;
+
+    private Long getRecommendShopId() {
+        return circuitBreakerFactory
+                .create(CircuitLog.SHOP_CIRCUIT_LOG.getValue())
+                .run(
+                        advertisementFeignService::getDrawShopId,
+                        throwable -> null
+                );
+    }
 
     public ShopResponse getShopById(Long shopId) {
         return ShopMapper.entityToDtoDetail(shopRepository.findOneById(shopId));
+    }
+
+    public List<ShopResponse> getHomePage(Long lastId, int pageSize) {
+        Long recommendShopId = getRecommendShopId();
+        return shopRepository.findHomePage(recommendShopId, lastId, pageSize);
     }
 }
