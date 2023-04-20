8bbdd8332c007514089f9a24100212d58d1f4e19
liveforone <yc4852@gmail.com>
2023-04-20 12:07:34 +0900

    Add shop repos util class.

---
+++ shop-service/shop-service/src/main/java/middle/shopservice/dto/ShopRepositoryUtil.java
@@ -0,0 +1,32 @@
+package middle.shopservice.dto;
+
+import com.querydsl.core.types.ConstructorExpression;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import middle.shopservice.domain.QShop;
+
+public class ShopRepositoryUtil {
+
+    public static final int ZERO_INDEX = 0;
+    static QShop shop = QShop.shop;
+
+    public static BooleanExpression ltShopId(Long lastId) {
+        if (lastId == 0) {
+            return null;
+        }
+
+        return shop.id.lt(lastId);
+    }
+
+    public static ConstructorExpression<ShopResponse> shopResponseConstructor() {
+        return Projections.constructor(ShopResponse.class,
+                shop.id,
+                shop.shopName,
+                shop.tel,
+                shop.city,
+                shop.street,
+                shop.detail,
+                shop.good,
+                shop.bad);
+    }
+}
