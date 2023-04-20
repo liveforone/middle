4f6bec9f7cf48f279273f8ebf26db3f4b9b0a721
liveforone <yc4852@gmail.com>
2023-04-20 16:59:02 +0900

    Add last id and page size constnat.

--- shop-service/shop-service/src/main/java/middle/shopservice/controller/constant/ParamConstant.java
+++ shop-service/shop-service/src/main/java/middle/shopservice/controller/constant/ParamConstant.java
@@ -3,4 +3,6 @@ package middle.shopservice.controller.constant;
 public class ParamConstant {
 
     public static final String SHOP_ID = "shopId";
+    public static final String LAST_ID = "lastId";
+    public static final String PAGE_SIZE = "pageSize";
 }
