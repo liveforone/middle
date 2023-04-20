0b209891d7a355e79f5d2d5ed5b60b50f3643412
liveforone <yc4852@gmail.com>
2023-04-20 17:30:25 +0900

    Add search shop name url.

--- shop-service/shop-service/src/main/java/middle/shopservice/controller/constant/ShopUrl.java
+++ shop-service/shop-service/src/main/java/middle/shopservice/controller/constant/ShopUrl.java
@@ -5,4 +5,5 @@ public class ShopUrl {
     public static final String SHOP_DETAIL = "/shop/{shopId}";
     public static final String MY_SHOP = "/my-shop";
     public static final String HOME_PAGE = "/shop/home";
+    public static final String SEARCH_SHOP_NAME = "/search/name/{name}";
 }
