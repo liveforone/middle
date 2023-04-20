b4cc2d0a01471a98b0224b73029b05b16c98bb05
liveforone <yc4852@gmail.com>
2023-04-20 12:13:17 +0900

    Add get shop by id service method.

--- shop-service/shop-service/src/main/java/middle/shopservice/service/ShopService.java
+++ shop-service/shop-service/src/main/java/middle/shopservice/service/ShopService.java
@@ -1,7 +1,9 @@
 package middle.shopservice.service;
 
 import lombok.RequiredArgsConstructor;
+import middle.shopservice.dto.ShopResponse;
 import middle.shopservice.repository.ShopRepository;
+import middle.shopservice.service.util.ShopMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,5 +12,9 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class ShopService {
 
-    private ShopRepository shopRepository;
+    private final ShopRepository shopRepository;
+
+    public ShopResponse getShopById(Long shopId) {
+        return ShopMapper.entityToDtoDetail(shopRepository.findOneById(shopId));
+    }
 }
