c76c3a94d017e34c366f933bf3acf85da15c8a3d
liveforone <yc4852@gmail.com>
2023-04-20 10:54:50 +0900

    Add id field in response dto.

--- shop-service/shop-service/src/main/java/middle/shopservice/dto/ShopResponse.java
+++ shop-service/shop-service/src/main/java/middle/shopservice/dto/ShopResponse.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ShopResponse {
 
+    private Long id;
     private String shopName;
     private String tel;
     private String city;
