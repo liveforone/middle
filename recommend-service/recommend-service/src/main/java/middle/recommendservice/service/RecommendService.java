f86e20b87594cce6a2c7a080512dd8015a4ed6a6
liveforone <yc4852@gmail.com>
2023-04-22 14:41:05 +0900

    Add get recommend by username service method.

--- recommend-service/recommend-service/src/main/java/middle/recommendservice/service/RecommendService.java
+++ recommend-service/recommend-service/src/main/java/middle/recommendservice/service/RecommendService.java
@@ -2,7 +2,9 @@ package middle.recommendservice.service;
 
 import lombok.RequiredArgsConstructor;
 import middle.recommendservice.domain.Recommend;
+import middle.recommendservice.dto.RecommendResponse;
 import middle.recommendservice.repository.RecommendRepository;
+import middle.recommendservice.service.util.RecommendMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,6 +14,11 @@ import org.springframework.transaction.annotation.Transactional;
 public class RecommendService {
 
     private final RecommendRepository recommendRepository;
+
+    public RecommendResponse getRecommendByUsername(String username) {
+        Recommend recommend = recommendRepository.findOneByUsername(username);
+        return RecommendMapper.entityToDto(recommend);
+    }
 
     @Transactional
     public void createRecommend(Long shopId, String username) {
