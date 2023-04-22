9d51cf3ce377cd98a8e251719536e3324dfa453b
liveforone <yc4852@gmail.com>
2023-04-22 13:43:34 +0900

    Add create recommend service method.

--- recommend-service/recommend-service/src/main/java/middle/recommendservice/service/RecommendService.java
+++ recommend-service/recommend-service/src/main/java/middle/recommendservice/service/RecommendService.java
@@ -1,6 +1,7 @@
 package middle.recommendservice.service;
 
 import lombok.RequiredArgsConstructor;
+import middle.recommendservice.domain.Recommend;
 import middle.recommendservice.repository.RecommendRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,4 +12,11 @@ import org.springframework.transaction.annotation.Transactional;
 public class RecommendService {
 
     private final RecommendRepository recommendRepository;
+
+    @Transactional
+    public void createRecommend(Long shopId, String username) {
+        Recommend recommend = Recommend.builder().build();
+        recommend.createRecommend(shopId, username);
+        recommendRepository.save(recommend);
+    }
 }
