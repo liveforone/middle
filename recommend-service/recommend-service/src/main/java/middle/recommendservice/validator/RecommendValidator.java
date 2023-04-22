0019d17006c89b3b3baa4433695d56caa98a9df4
liveforone <yc4852@gmail.com>
2023-04-22 14:10:29 +0900

    Add duplicate check validator.

---
+++ recommend-service/recommend-service/src/main/java/middle/recommendservice/validator/RecommendValidator.java
@@ -0,0 +1,19 @@
+package middle.recommendservice.validator;
+
+import lombok.RequiredArgsConstructor;
+import middle.recommendservice.repository.RecommendRepository;
+import middle.recommendservice.utility.CommonUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RecommendValidator {
+
+    private final RecommendRepository recommendRepository;
+
+    public boolean isDuplicateRecommend(String username) {
+        Long foundId = recommendRepository.findOneIdByUsernameForValidation(username);
+
+        return !CommonUtils.isNull(foundId);
+    }
+}
