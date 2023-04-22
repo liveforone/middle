bdff778eb0d900c05ae9f9dc45c8152a9775513f
liveforone <yc4852@gmail.com>
2023-04-22 15:09:36 +0900

    Add spring validatoin in impression request dto.

--- recommend-service/recommend-service/src/main/java/middle/recommendservice/dto/ImpressionRequest.java
+++ recommend-service/recommend-service/src/main/java/middle/recommendservice/dto/ImpressionRequest.java
@@ -1,10 +1,13 @@
 package middle.recommendservice.dto;
 
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class ImpressionRequest {
+
+    @Positive(message = "추가하려는 노출횟수를 반드시 기입해주세요.")
     private long impression;
 }
