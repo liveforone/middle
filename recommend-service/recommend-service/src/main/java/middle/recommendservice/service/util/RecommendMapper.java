76e25f62ad7825755c16c18431dc5813b4267978
liveforone <yc4852@gmail.com>
2023-04-22 14:40:49 +0900

    Add null check in entity to dto mapper.

--- recommend-service/recommend-service/src/main/java/middle/recommendservice/service/util/RecommendMapper.java
+++ recommend-service/recommend-service/src/main/java/middle/recommendservice/service/util/RecommendMapper.java
@@ -2,10 +2,15 @@ package middle.recommendservice.service.util;
 
 import middle.recommendservice.domain.Recommend;
 import middle.recommendservice.dto.RecommendResponse;
+import middle.recommendservice.utility.CommonUtils;
 
 public class RecommendMapper {
 
     public static RecommendResponse entityToDto(Recommend recommend) {
+        if (CommonUtils.isNull(recommend)) {
+            return new RecommendResponse();
+        }
+
         return RecommendResponse.builder()
                 .id(recommend.getId())
                 .shopId(recommend.getShopId())
