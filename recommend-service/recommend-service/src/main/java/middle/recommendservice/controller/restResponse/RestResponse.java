f8d32fb5d90a79762c7d42c18cb380fa31d29bdf
liveforone <yc4852@gmail.com>
2023-04-22 14:19:27 +0900

    Add create recommend success rest response and message.

---
+++ recommend-service/recommend-service/src/main/java/middle/recommendservice/controller/restResponse/RestResponse.java
@@ -0,0 +1,25 @@
+package middle.recommendservice.controller.restResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class RestResponse {
+
+    public static ResponseEntity<?> duplicateRecommend() {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(RestMessage.DUPLICATE_RECOMMEND.getValue());
+    }
+
+    public static ResponseEntity<?> shopIsNull() {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(RestMessage.SHOP_IS_NULL.getValue());
+    }
+
+    public static ResponseEntity<?> createRecommendSuccess() {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(RestMessage.CREATE_RECOMMEND_SUCCESS.getValue());
+    }
+}
