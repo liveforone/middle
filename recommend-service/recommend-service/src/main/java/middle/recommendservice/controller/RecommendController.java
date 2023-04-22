15a3ec96f5426c77fc2e4da88f4c786f561b2670
liveforone <yc4852@gmail.com>
2023-04-22 14:20:09 +0900

    Add create recommend controller.

--- recommend-service/recommend-service/src/main/java/middle/recommendservice/controller/RecommendController.java
+++ recommend-service/recommend-service/src/main/java/middle/recommendservice/controller/RecommendController.java
@@ -1,11 +1,20 @@
 package middle.recommendservice.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import middle.recommendservice.authentication.AuthenticationInfo;
+import middle.recommendservice.controller.constant.ControllerLog;
+import middle.recommendservice.controller.constant.RecommendUrl;
+import middle.recommendservice.controller.restResponse.RestResponse;
 import middle.recommendservice.feignClient.ShopFeignService;
 import middle.recommendservice.feignClient.constant.CircuitLog;
 import middle.recommendservice.service.RecommendService;
+import middle.recommendservice.utility.CommonUtils;
+import middle.recommendservice.validator.RecommendValidator;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +25,8 @@ public class RecommendController {
     private final RecommendService recommendService;
     private final ShopFeignService shopFeignService;
     private final CircuitBreakerFactory<?, ?> circuitBreakerFactory;
+    private final AuthenticationInfo authenticationInfo;
+    private final RecommendValidator recommendValidator;
 
     private Long getShopByUsername(String username) {
         return circuitBreakerFactory
@@ -24,4 +35,26 @@ public class RecommendController {
                         throwable -> null
                 );
     }
+
+    @PostMapping(RecommendUrl.CREATE_RECOMMEND)
+    public ResponseEntity<?> createRecommend(
+            HttpServletRequest request
+    ) {
+        String username = authenticationInfo.getUsername(request);
+        if (recommendValidator.isDuplicateRecommend(username)) {
+            return RestResponse.duplicateRecommend();
+        }
+
+        Long shopId = getShopByUsername(username);
+        if (CommonUtils.isNull(shopId)) {
+            return RestResponse.shopIsNull();
+        }
+
+        recommendService.createRecommend(shopId, username);
+        log.info(ControllerLog.CREATE_RECOMMEND_SUCCESS.getValue());
+
+        return RestResponse.createRecommendSuccess();
+    }
+
+    //switch 문에서 하나씩 검증해보고, 만약에 없는 값이라서 default까지 가면 badrequest 날려야함
 }
