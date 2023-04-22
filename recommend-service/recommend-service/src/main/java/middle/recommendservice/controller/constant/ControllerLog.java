d206d13355a69007656a5b1642fa04cf39ee2ddc
liveforone <yc4852@gmail.com>
2023-04-22 15:37:07 +0900

    Add increase impression success controller log.

--- recommend-service/recommend-service/src/main/java/middle/recommendservice/controller/constant/ControllerLog.java
+++ recommend-service/recommend-service/src/main/java/middle/recommendservice/controller/constant/ControllerLog.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ControllerLog {
 
-    CREATE_RECOMMEND_SUCCESS("추천 서비스에 성공적으로 등록했습니다.");
+    CREATE_RECOMMEND_SUCCESS("추천 서비스에 성공적으로 등록했습니다."),
+    INCREASE_IMPRESSION_SUCCESS("노출횟수를 성공적으로 추가했습니다.");
 
     private final String value;
 }
