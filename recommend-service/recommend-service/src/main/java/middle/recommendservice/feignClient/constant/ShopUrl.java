10e5a906f7e2d9f750ca49d0e63e9620dac5c594
liveforone <yc4852@gmail.com>
2023-04-22 12:36:12 +0900

    Add shop url.

---
+++ recommend-service/recommend-service/src/main/java/middle/recommendservice/feignClient/constant/ShopUrl.java
@@ -0,0 +1,7 @@
+package middle.recommendservice.feignClient.constant;
+
+public class ShopUrl {
+
+    public static final String BASE = "recommend-service";
+    public static final String RETURN_SHOP_USERNAME = "/shop/username/{username}";
+}
