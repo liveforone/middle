8017834396d0a29adabe0478d1b3464e609e8db1
liveforone <yc4852@gmail.com>
2023-04-22 12:36:38 +0900

    Add shop feign service.

---
+++ recommend-service/recommend-service/src/main/java/middle/recommendservice/feignClient/ShopFeignService.java
@@ -0,0 +1,14 @@
+package middle.recommendservice.feignClient;
+
+import middle.recommendservice.feignClient.constant.ShopParamConstant;
+import middle.recommendservice.feignClient.constant.ShopUrl;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@FeignClient(ShopUrl.BASE)
+public interface ShopFeignService {
+
+    @PostMapping(ShopUrl.RETURN_SHOP_USERNAME)
+    Long getShopByUsername(@PathVariable(ShopParamConstant.USERNAME) String username);
+}
