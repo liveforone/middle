be6078028155fbce81df5ba8696b884df6de940a
liveforone <yc4852@gmail.com>
2023-04-22 12:05:04 +0900

    Add jwt authentication config.

---
+++ recommend-service/recommend-service/src/main/java/middle/recommendservice/authentication/AuthenticationInfo.java
@@ -0,0 +1,58 @@
+package middle.recommendservice.authentication;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import middle.recommendservice.authentication.constant.JwtConstant;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.security.Key;
+import java.util.Base64;
+
+@Component
+public class AuthenticationInfo {
+
+    private final Key key;
+
+    public AuthenticationInfo(@Value(JwtConstant.SECRET_KEY_PATH) String secretKey) {
+        byte[] keyBytes = Base64.getDecoder().decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String getUsername(HttpServletRequest request) {
+        String token = resolveToken(request);
+        Claims claims = getAuthentication(token);
+        return claims.getSubject();
+    }
+
+    public String getAuth(HttpServletRequest request) {
+        String token = resolveToken(request);
+        Claims claims = getAuthentication(token);
+        return claims.get(JwtConstant.CLAIM_NAME).toString();
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(JwtConstant.HEADER);
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(JwtConstant.BEARER_TOKEN)) {
+            return bearerToken.substring(JwtConstant.TOKEN_SUB_INDEX);
+        }
+        return null;
+    }
+
+    private Claims parseClaims(String accessToken) {
+        try {
+            return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(accessToken).getBody();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
+    }
+
+    private Claims getAuthentication(String accessToken) {
+        return parseClaims(accessToken);
+    }
+}
