5012f92e8408212455e883edf3c25fadb0376f2e
liveforone <yc4852@gmail.com>
2023-04-22 16:46:41 +0900

    Add start id constant in recommend algorithm.

--- recommend-service/recommend-service/src/main/java/middle/recommendservice/algorithm/RecommendAlgorithm.java
+++ recommend-service/recommend-service/src/main/java/middle/recommendservice/algorithm/RecommendAlgorithm.java
@@ -12,11 +12,13 @@ public class RecommendAlgorithm {
 
     private final RecommendRepository recommendRepository;
 
+    private final static int START_ID = 1;
+
     public Long recommendShop() {
         Long sizeOfRecommend = recommendRepository.countSizeOfRecommend();
         Random random = new Random();
         random.setSeed(System.currentTimeMillis());
 
-        return Integer.toUnsignedLong(random.nextInt(Math.toIntExact(sizeOfRecommend)) + 1);
+        return Integer.toUnsignedLong(random.nextInt(Math.toIntExact(sizeOfRecommend)) + START_ID);
     }
 }
