5f754f2bb6763c5b5f2895367cee08fa45423432
liveforone <yc4852@gmail.com>
2023-04-22 14:24:27 +0900

    Add my recommend url.

--- recommend-service/recommend-service/src/main/java/middle/recommendservice/controller/constant/RecommendUrl.java
+++ recommend-service/recommend-service/src/main/java/middle/recommendservice/controller/constant/RecommendUrl.java
@@ -2,4 +2,5 @@ package middle.recommendservice.controller.constant;
 
 public class RecommendUrl {
     public static final String CREATE_RECOMMEND = "/create-recommend";
+    public static final String MY_RECOMMEND = "/my-recommend";
 }
