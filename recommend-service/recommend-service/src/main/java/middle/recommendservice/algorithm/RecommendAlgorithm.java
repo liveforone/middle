ce39b09f0cfb74e849cd90cc07e9bbff52089c59
liveforone <yc4852@gmail.com>
2023-04-22 16:33:51 +0900

    Add recommend shop algorithm.

--- recommend-service/recommend-service/src/main/java/middle/recommendservice/algorithm/RecommendAlgorithm.java
+++ recommend-service/recommend-service/src/main/java/middle/recommendservice/algorithm/RecommendAlgorithm.java
@@ -4,11 +4,19 @@ import lombok.RequiredArgsConstructor;
 import middle.recommendservice.repository.RecommendRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 @Component
 @RequiredArgsConstructor
 public class RecommendAlgorithm {
 
     private final RecommendRepository recommendRepository;
 
+    public Long recommendShop() {
+        Long sizeOfRecommend = recommendRepository.countSizeOfRecommend();
+        Random random = new Random();
+        random.setSeed(System.currentTimeMillis());
 
+        return Integer.toUnsignedLong(random.nextInt(Math.toIntExact(sizeOfRecommend)) + 1);
+    }
 }
