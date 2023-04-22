bc216087c7f96f4fb86e2667248b0de56350d4db
liveforone <yc4852@gmail.com>
2023-04-22 12:40:38 +0900

    Add circuit log.

---
+++ recommend-service/recommend-service/src/main/java/middle/recommendservice/feignClient/constant/CircuitLog.java
@@ -0,0 +1,13 @@
+package middle.recommendservice.feignClient.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum CircuitLog {
+
+    SHOP_CIRCUIT("shop-circuit");
+
+    private final String value;
+}
