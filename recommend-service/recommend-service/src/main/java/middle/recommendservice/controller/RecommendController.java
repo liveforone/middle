2ad3b8eca4c8e13fa90d7d94c4410f6bbb5b3780
liveforone <yc4852@gmail.com>
2023-04-22 12:40:53 +0900

    Add circuit breaker and feign in controller.

--- recommend-service/recommend-service/src/main/java/middle/recommendservice/controller/RecommendController.java
+++ recommend-service/recommend-service/src/main/java/middle/recommendservice/controller/RecommendController.java
@@ -2,7 +2,10 @@ package middle.recommendservice.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import middle.recommendservice.feignClient.ShopFeignService;
+import middle.recommendservice.feignClient.constant.CircuitLog;
 import middle.recommendservice.service.RecommendService;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class RecommendController {
 
     private final RecommendService recommendService;
+    private final ShopFeignService shopFeignService;
+    private final CircuitBreakerFactory<?, ?> circuitBreakerFactory;
+
+    private Long getShopByUsername(String username) {
+        return circuitBreakerFactory
+                .create(CircuitLog.SHOP_CIRCUIT.getValue())
+                .run(() -> shopFeignService.getShopByUsername(username),
+                        throwable -> null
+                );
+    }
 }
