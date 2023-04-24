b97a45422e5fb024f5b6ed4cce0fb28490937a04
liveforone <yc4852@gmail.com>
2023-04-24 21:39:20 +0900

    Add cache enable.

--- recommend-service/recommend-service/src/main/java/middle/recommendservice/RecommendServiceApplication.java
+++ recommend-service/recommend-service/src/main/java/middle/recommendservice/RecommendServiceApplication.java
@@ -3,6 +3,7 @@ package middle.recommendservice;
 import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,7 @@ import org.springframework.web.filter.HiddenHttpMethodFilter;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCaching
 public class RecommendServiceApplication {
 
 	public static void main(String[] args) {
