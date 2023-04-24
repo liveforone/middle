ab837b0ba132adf33747cfaaf5afdd4c680def14
liveforone <yc4852@gmail.com>
2023-04-24 22:41:02 +0900

    Remove cache.

--- recommend-service/recommend-service/src/main/java/middle/recommendservice/cache/CacheConstant.java
+++
@@ -1,7 +0,0 @@
-package middle.recommendservice.cache;
-
-public class CacheConstant {
-
-    public static final String COUNT_RECOMMEND_NAME = "countRecommendCache";
-    public static final String COUNT_RECOMMEND = "countRecommend";
-}
