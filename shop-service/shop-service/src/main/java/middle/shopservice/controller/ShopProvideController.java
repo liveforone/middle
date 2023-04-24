b6f18fdb9e45df537df5504aee9e094438b9a04f
liveforone <yc4852@gmail.com>
2023-04-24 22:40:20 +0900

    Remove unusing dependency.

--- shop-service/shop-service/src/main/java/middle/shopservice/controller/ShopProvideController.java
+++ shop-service/shop-service/src/main/java/middle/shopservice/controller/ShopProvideController.java
@@ -3,7 +3,6 @@ package middle.shopservice.controller;
 import lombok.RequiredArgsConstructor;
 import middle.shopservice.controller.constant.ParamConstant;
 import middle.shopservice.controller.constant.ShopUrl;
-import middle.shopservice.dto.ShopResponse;
 import middle.shopservice.service.ShopService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
