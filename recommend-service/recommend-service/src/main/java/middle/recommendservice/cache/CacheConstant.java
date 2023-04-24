5a033dafac0cd2014c58f7a69b18893b335af967
liveforone <yc4852@gmail.com>
2023-04-24 21:38:58 +0900

    Add cache constant.

---
+++ recommend-service/recommend-service/src/main/java/middle/recommendservice/cache/CacheConstant.java
@@ -0,0 +1,7 @@
+package middle.recommendservice.cache;
+
+public class CacheConstant {
+
+    public static final String COUNT_RECOMMEND_NAME = "countRecommendCache";
+    public static final String COUNT_RECOMMEND = "countRecommend";
+}
