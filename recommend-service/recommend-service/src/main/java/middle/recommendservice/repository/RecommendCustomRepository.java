dfdd88b511c811351db54f69db1de97a4dec008e
liveforone <yc4852@gmail.com>
2023-04-24 21:39:44 +0900

    Add cacheable annotation in count recommend query method.

--- recommend-service/recommend-service/src/main/java/middle/recommendservice/repository/RecommendCustomRepository.java
+++ recommend-service/recommend-service/src/main/java/middle/recommendservice/repository/RecommendCustomRepository.java
@@ -1,11 +1,14 @@
 package middle.recommendservice.repository;
 
+import middle.recommendservice.cache.CacheConstant;
 import middle.recommendservice.domain.Recommend;
+import org.springframework.cache.annotation.Cacheable;
 
 public interface RecommendCustomRepository {
 
     Long findOneIdByUsernameForValidation(String username);
     Recommend findOneByUsername(String username);
+    @Cacheable(cacheNames = CacheConstant.COUNT_RECOMMEND_NAME, value = CacheConstant.COUNT_RECOMMEND)
     Long countSizeOfRecommend();
     Recommend findOneByShopId(Long shopId);
 }
