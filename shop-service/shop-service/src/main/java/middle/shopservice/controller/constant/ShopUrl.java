cc30e39445588bb1f91624795da7433c5b18de29
liveforone <yc4852@gmail.com>
2023-04-25 14:27:40 +0900

    Add combine search url and remove others.

--- shop-service/shop-service/src/main/java/middle/shopservice/controller/constant/ShopUrl.java
+++ shop-service/shop-service/src/main/java/middle/shopservice/controller/constant/ShopUrl.java
@@ -5,9 +5,7 @@ public class ShopUrl {
     public static final String SHOP_DETAIL = "/shop/{shopId}";
     public static final String MY_SHOP = "/my-shop";
     public static final String HOME_PAGE = "/shop/home";
-    public static final String SEARCH_SHOP_NAME = "/search/name/{name}";
-    public static final String SEARCH_CITY = "/search/city/{city}";
-    public static final String SEARCH_STREET = "/search/street/{street}";
+    public static final String SEARCH_SHOP = "/search";
     public static final String CREATE_SHOP = "/create-shop";
     public static final String UPDATE_SHOP_NAME = "/update/name";
     public static final String UPDATE_SHOP_TEL = "/update/tel";
