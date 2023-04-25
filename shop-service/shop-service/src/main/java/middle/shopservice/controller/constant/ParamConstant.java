46b20399e1f9abbf9ff9bfa7673654727268b9eb
liveforone <yc4852@gmail.com>
2023-04-25 14:26:40 +0900

    Add default last id.

--- shop-service/shop-service/src/main/java/middle/shopservice/controller/constant/ParamConstant.java
+++ shop-service/shop-service/src/main/java/middle/shopservice/controller/constant/ParamConstant.java
@@ -4,6 +4,7 @@ public class ParamConstant {
 
     public static final String SHOP_ID = "shopId";
     public static final String LAST_ID = "lastId";
+    public static final String DEFAULT_ID = "0";
     public static final String SHOP_NAME = "name";
     public static final String CITY = "city";
     public static final String STREET = "street";
