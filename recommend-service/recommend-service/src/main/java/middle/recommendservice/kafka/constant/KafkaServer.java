d4cba66d593db864238c02a460a2e0f64602ab9f
liveforone <yc4852@gmail.com>
2023-04-27 14:18:07 +0900

    Add kafka server constant.

---
+++ recommend-service/recommend-service/src/main/java/middle/recommendservice/kafka/constant/KafkaServer.java
@@ -0,0 +1,7 @@
+package middle.recommendservice.kafka.constant;
+
+public class KafkaServer {
+
+    public static final String KAFKA_SERVER_POST = "localhost:9092";
+//    public static final String KAFKA_SERVER_POST = "172.18.0.101:9092";
+}
