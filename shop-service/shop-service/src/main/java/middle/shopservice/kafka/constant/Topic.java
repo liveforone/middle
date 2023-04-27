b176650ee5f8006890725192f8c77008c802b7f6
liveforone <yc4852@gmail.com>
2023-04-27 13:58:45 +0900

    Add remove recommend topic.

--- shop-service/shop-service/src/main/java/middle/shopservice/kafka/constant/Topic.java
+++ shop-service/shop-service/src/main/java/middle/shopservice/kafka/constant/Topic.java
@@ -5,5 +5,6 @@ public class Topic {
     public static final String SHOP_IS_GOOD = "shop-is-good";
     public static final String SHOP_IS_BAD = "shop-is-bad";
     public static final String REMOVE_SHOP_BELONG_MEMBER = "remove-shop-belong-member";
+    public static final String REMOVE_RECOMMEND = "remove-recommend";
     public static final String REMOVE_TIMETABLE = "remove-timetable";
 }
