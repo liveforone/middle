f7b19ee7e882302d695320497c8865f4c55433bb
liveforone <yc4852@gmail.com>
2023-04-27 14:18:13 +0900

    Add kafka log.

---
+++ recommend-service/recommend-service/src/main/java/middle/recommendservice/kafka/constant/KafkaLog.java
@@ -0,0 +1,15 @@
+package middle.recommendservice.kafka.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum KafkaLog {
+
+    KAFKA_RECEIVE_LOG("Consumer receive Kafka Message -> "),
+    KAFKA_NULL_LOG("!! Kafka Message is Null !!"),
+    REMOVE_RECOMMEND("Remove Recommend Belong Shop | shop id : ");
+
+    private final String value;
+}
