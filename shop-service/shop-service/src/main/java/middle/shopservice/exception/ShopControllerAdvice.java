4f21db54e3540e27175d19841aeda76507624005
liveforone <yc4852@gmail.com>
2023-04-30 21:21:37 +0900

    Add shop controller advice.

---
+++ shop-service/shop-service/src/main/java/middle/shopservice/exception/ShopControllerAdvice.java
@@ -0,0 +1,24 @@
+package middle.shopservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ShopControllerAdvice {
+
+    @ExceptionHandler(ShopCustomException.class)
+    protected ResponseEntity<?> memberCustomHandle(ShopCustomException customException) {
+        return ResponseEntity
+                .status(customException.getResponseMessage().getStatus())
+                .body(customException.getResponseMessage().getValue());
+    }
+
+    @ExceptionHandler(BindingCustomException.class)
+    protected ResponseEntity<?> bindingErrorHandle(BindingCustomException customException) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(customException.getMessage());
+    }
+}
