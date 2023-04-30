8bf85ea2a0cf6a39bca6ddec7bd08dff4e4b5b58
liveforone <yc4852@gmail.com>
2023-04-30 20:51:32 +0900

    Add custom exception in validator and change return type to void for functional style.

--- user-service/user-service/src/main/java/middle/userservice/validator/MemberValidator.java
+++ user-service/user-service/src/main/java/middle/userservice/validator/MemberValidator.java
@@ -1,41 +1,68 @@
 package middle.userservice.validator;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import middle.userservice.controller.constant.ControllerLog;
+import middle.userservice.controller.restResponse.ResponseMessage;
 import middle.userservice.domain.Member;
 import middle.userservice.domain.Role;
+import middle.userservice.exception.BindingCustomException;
+import middle.userservice.exception.MemberCustomException;
+import middle.userservice.kafka.UserProducer;
 import middle.userservice.repository.MemberRepository;
 import middle.userservice.utility.CommonUtils;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
 
 import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class MemberValidator {
 
     private final MemberRepository memberRepository;
+    private final UserProducer userProducer;
 
     static BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-    public boolean isNotMatchingPassword(String inputPassword, String username) {
+    public void validatePassword(String inputPassword, String username) {
         Member foundMember = memberRepository.findByUsername(username);
         String originalPassword = foundMember.getPassword();
 
-        return !passwordEncoder.matches(inputPassword, originalPassword);
+        if (!passwordEncoder.matches(inputPassword, originalPassword)) {
+            throw new MemberCustomException(ResponseMessage.NOT_MATCH_PASSWORD);
+        }
     }
 
-    public boolean isDuplicateEmail(String email) {
+    public void validateBinding(BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            String errorMessage = Objects
+                    .requireNonNull(bindingResult.getFieldError())
+                    .getDefaultMessage();
+            throw new BindingCustomException(errorMessage);
+        }
+    }
+
+    public void validateDuplicateEmail(String email) {
         Member member = memberRepository.findByEmail(email);
 
-        return !CommonUtils.isNull(member);
+        if (!CommonUtils.isNull(member)) {
+            throw new MemberCustomException(ResponseMessage.DUPLICATE_EMAIL);
+        }
     }
 
-    public boolean isOwner(String auth) {
-        return Objects.equals(auth, Role.OWNER.getValue());
+    public void validateAuth(String auth, String username) {
+        if (Objects.equals(auth, Role.OWNER.getValue())) {
+            userProducer.removeShopBelongMember(username);
+        }
     }
 
-    public boolean isOwner(Role auth) {
-        return auth.equals(Role.ADMIN);
+    public void validateAuth(Role auth) {
+        if (!auth.equals(Role.ADMIN)) {
+            log.error(ControllerLog.ADMIN_FAIL.getValue());
+            throw new MemberCustomException(ResponseMessage.PROHIBITION);
+        }
     }
 }
