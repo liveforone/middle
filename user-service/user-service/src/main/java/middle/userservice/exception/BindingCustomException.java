f492b3b5bc92260108cc2cc814e3612ec7598a78
liveforone <yc4852@gmail.com>
2023-04-30 20:51:00 +0900

    Add binding custom exception class.

---
+++ user-service/user-service/src/main/java/middle/userservice/exception/BindingCustomException.java
@@ -0,0 +1,8 @@
+package middle.userservice.exception;
+
+public class BindingCustomException extends RuntimeException{
+
+    public BindingCustomException(String message) {
+        super(message);
+    }
+}
