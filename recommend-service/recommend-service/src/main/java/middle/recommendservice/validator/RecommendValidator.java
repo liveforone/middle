ca1e203440d5b39287f96f2d6561aed2c9ba0913
liveforone <yc4852@gmail.com>
2023-04-30 21:49:51 +0900

    Refactor return type to void for functional style.

--- recommend-service/recommend-service/src/main/java/middle/recommendservice/validator/RecommendValidator.java
+++ recommend-service/recommend-service/src/main/java/middle/recommendservice/validator/RecommendValidator.java
@@ -1,9 +1,15 @@
 package middle.recommendservice.validator;
 
 import lombok.RequiredArgsConstructor;
+import middle.recommendservice.controller.restResponse.ResponseMessage;
+import middle.recommendservice.exception.BindingCustomException;
+import middle.recommendservice.exception.RecommendCustomException;
 import middle.recommendservice.repository.RecommendRepository;
 import middle.recommendservice.utility.CommonUtils;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
@@ -11,15 +17,34 @@ public class RecommendValidator {
 
     private final RecommendRepository recommendRepository;
 
-    public boolean isNotExistRecommend(String username) {
-        Long foundId = recommendRepository.findOneIdByUsernameForValidation(username);
-
-        return CommonUtils.isNull(foundId);
+    public void validateBinding(BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            String errorMessage = Objects
+                    .requireNonNull(bindingResult.getFieldError())
+                    .getDefaultMessage();
+            throw new BindingCustomException(errorMessage);
+        }
     }
 
-    public boolean isDuplicateRecommend(String username) {
+    public void validateRecommendNull(String username) {
         Long foundId = recommendRepository.findOneIdByUsernameForValidation(username);
 
-        return !CommonUtils.isNull(foundId);
+        if (CommonUtils.isNull(foundId)) {
+            throw new RecommendCustomException(ResponseMessage.RECOMMEND_IS_NULL);
+        }
+    }
+
+    public void validateShopNull(Long shopId) {
+        if (CommonUtils.isNull(shopId)) {
+            throw new RecommendCustomException(ResponseMessage.SHOP_IS_NULL);
+        }
+    }
+
+    public void validateDuplicateRecommend(String username) {
+        Long foundId = recommendRepository.findOneIdByUsernameForValidation(username);
+
+        if (!CommonUtils.isNull(foundId)) {
+            throw new RecommendCustomException(ResponseMessage.DUPLICATE_RECOMMEND);
+        }
     }
 }
