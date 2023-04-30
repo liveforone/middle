bf197550669c46edf4689bc8a30c598c896d0565
liveforone <yc4852@gmail.com>
2023-04-30 21:50:00 +0900

    Add recommend controller advice.

---
+++ recommend-service/recommend-service/src/main/java/middle/recommendservice/exception/RecommendControllerAdvice.java
@@ -0,0 +1,24 @@
+package middle.recommendservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RecommendControllerAdvice {
+
+    @ExceptionHandler(RecommendCustomException.class)
+    protected ResponseEntity<?> memberCustomHandle(RecommendCustomException customException) {
+        return ResponseEntity
+                .status(customException.getResponseMessage().getStatus())
+                .body(customException.getResponseMessage().getValue());
+    }
+
+    @ExceptionHandler(BindingCustomException.class)
+    protected ResponseEntity<?> bindingErrorHandle(BindingCustomException customException) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(customException.getMessage());
+    }
+}
