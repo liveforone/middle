d6b0c2c1c877281586117a731ee553b59594b561
liveforone <yc4852@gmail.com>
2023-05-01 21:37:40 +0900

    Add feign client in main method.

--- timetable-service/timetable-service/src/main/java/middle/timetableservice/TimetableServiceApplication.java
+++ timetable-service/timetable-service/src/main/java/middle/timetableservice/TimetableServiceApplication.java
@@ -1,14 +1,17 @@
 package middle.timetableservice;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 @EnableScheduling
 public class TimetableServiceApplication {
 
@@ -19,5 +22,10 @@ public class TimetableServiceApplication {
 	@Bean
 	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
 		return new HiddenHttpMethodFilter();
+	}
+
+	@Bean
+	public Logger.Level feignLoggerLeve() {
+		return Logger.Level.FULL;
 	}
 }
