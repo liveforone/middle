1003117bd13f0574415d8829d248dacdee47bc27
liveforone <yc4852@gmail.com>
2023-05-01 19:56:04 +0900

    Relocate provide url to shop service.

--- recommend-service/recommend-service/src/main/java/middle/recommendservice/controller/ProvideUrl.java
+++
@@ -1,6 +0,0 @@
-package middle.recommendservice.controller;
-
-public class ProvideUrl {
-
-    public static final String RECOMMEND_SHOP = "/recommend-shop";
-}
---
+++ recommend-service/recommend-service/src/main/java/middle/recommendservice/provideController/constant/ProvideUrlToShop.java
@@ -0,0 +1,6 @@
+package middle.recommendservice.provideController.constant;
+
+public final class ProvideUrlToShop {
+
+    public static final String RECOMMEND_SHOP = "/recommend-shop";
+}
