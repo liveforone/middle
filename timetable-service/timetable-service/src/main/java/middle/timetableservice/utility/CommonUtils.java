cda9161063b80e917acc4ff0ac5d85ffdc2ea791
liveforone <yc4852@gmail.com>
2023-05-01 21:34:14 +0900

    Add common util class.

---
+++ timetable-service/timetable-service/src/main/java/middle/timetableservice/utility/CommonUtils.java
@@ -0,0 +1,26 @@
+package middle.timetableservice.utility;
+
+import java.util.List;
+
+public class CommonUtils {
+
+    public static boolean isNull(Object obj) {
+
+        //일반 객체 체크
+        if(obj == null) {
+            return true;
+        }
+
+        //문자열 체크
+        if ((obj instanceof String) && (((String)obj).trim().length() == 0)) {
+            return true;
+        }
+
+        //리스트 체크
+        if (obj instanceof List) {
+            return ((List<?>)obj).isEmpty();
+        }
+
+        return false;
+    }
+}
