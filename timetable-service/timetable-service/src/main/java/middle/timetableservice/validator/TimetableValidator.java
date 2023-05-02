70b6cd7b2fbf2782581b3d0b30d754562e6a87a1
liveforone <yc4852@gmail.com>
2023-05-02 14:52:03 +0900

    Add validate auth validator.

--- timetable-service/timetable-service/src/main/java/middle/timetableservice/validator/TimetableValidator.java
+++ timetable-service/timetable-service/src/main/java/middle/timetableservice/validator/TimetableValidator.java
@@ -1,11 +1,14 @@
 package middle.timetableservice.validator;
 
 import lombok.RequiredArgsConstructor;
+import middle.timetableservice.authentication.Role;
 import middle.timetableservice.controller.restResponse.ResponseMessage;
 import middle.timetableservice.exception.TimetableCustomException;
 import middle.timetableservice.repository.TimetableRepository;
 import middle.timetableservice.utility.CommonUtils;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
@@ -20,4 +23,12 @@ public class TimetableValidator {
             throw new TimetableCustomException(ResponseMessage.TIMETABLE_IS_NULL);
         }
     }
+
+    public void validateAuth(String auth) {
+        if (!Objects.equals(auth, Role.OWNER.getValue())) {
+            throw new TimetableCustomException(ResponseMessage.AUTH_IS_NOT_OWNER);
+        }
+    }
+
+
 }
