c28dbaf702764a0d694c999ef4021bff1e1ad2e8
liveforone <yc4852@gmail.com>
2023-05-02 13:51:20 +0900

    Add timetable is null response message.

--- timetable-service/timetable-service/src/main/java/middle/timetableservice/controller/restResponse/ResponseMessage.java
+++ timetable-service/timetable-service/src/main/java/middle/timetableservice/controller/restResponse/ResponseMessage.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ResponseMessage {
 
+    TIMETABLE_IS_NULL(404, "타임테이블이 존재하지 않습니다."),
     CREATE_TIMETABLE_SUCCESS(201, "타임테이블 생성을 성공적으로 완료했습니다.");
 
     private final int status;
