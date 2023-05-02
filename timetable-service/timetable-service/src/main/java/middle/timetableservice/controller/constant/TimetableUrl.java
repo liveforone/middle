7af36b3329e6af213a661c3286e9482bed6352f3
liveforone <yc4852@gmail.com>
2023-05-02 13:43:31 +0900

    Add find one and page url.

--- timetable-service/timetable-service/src/main/java/middle/timetableservice/controller/constant/TimetableUrl.java
+++ timetable-service/timetable-service/src/main/java/middle/timetableservice/controller/constant/TimetableUrl.java
@@ -3,4 +3,5 @@ package middle.timetableservice.controller.constant;
 public final class TimetableUrl {
 
     public static final String TIMETABLE_PAGE_BY_SHOP = "/timetables/{shopId}";
+    public static final String TIMETABLE_DETAIL = "/timetable/detail/{id}";
 }
