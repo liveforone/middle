418bbcfd20da3cd6cd7a99d93c7687fe3653d5a2
liveforone <yc4852@gmail.com>
2023-05-02 16:39:53 +0900

    Add delete success response message.

--- timetable-service/timetable-service/src/main/java/middle/timetableservice/controller/restResponse/ResponseMessage.java
+++ timetable-service/timetable-service/src/main/java/middle/timetableservice/controller/restResponse/ResponseMessage.java
@@ -12,7 +12,8 @@ public enum ResponseMessage {
     SHOP_IS_NULL(404, "상점이 존재하지 않습니다."),
     NOT_OWNER_OF_SHOP(400, "상점의 주인이 아닙니다."),
     CREATE_TIMETABLE_SUCCESS(201, "타임테이블 생성을 성공적으로 완료했습니다."),
-    UPDATE_TIME_SUCCESS(200, "시간을 성공적으로 업데이트 했습니다.");
+    UPDATE_TIME_SUCCESS(200, "시간을 성공적으로 업데이트 했습니다."),
+    DELETE_TIMETABLE_SUCCESS(200, "타임테이블을 성공적으로 삭제했습니다.");
 
     private final int status;
     private final String value;
