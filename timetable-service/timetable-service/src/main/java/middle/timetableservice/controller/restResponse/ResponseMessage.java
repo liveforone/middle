334aae1d57466624e9a089fc7af4c2e511595e3c
liveforone <yc4852@gmail.com>
2023-05-02 14:51:35 +0900

    Add shop is null and not owner of shop response emssage.

--- timetable-service/timetable-service/src/main/java/middle/timetableservice/controller/restResponse/ResponseMessage.java
+++ timetable-service/timetable-service/src/main/java/middle/timetableservice/controller/restResponse/ResponseMessage.java
@@ -9,6 +9,8 @@ public enum ResponseMessage {
 
     TIMETABLE_IS_NULL(404, "타임테이블이 존재하지 않습니다."),
     AUTH_IS_NOT_OWNER(403, "가게 주인 회원만 타임테이블 등록이 가능합니다."),
+    SHOP_IS_NULL(404, "상점이 존재하지 않습니다."),
+    NOT_OWNER_OF_SHOP(400, "상점의 주인이 아닙니다."),
     CREATE_TIMETABLE_SUCCESS(201, "타임테이블 생성을 성공적으로 완료했습니다.");
 
     private final int status;
