56ec76f1441dcdc66ca1a22bbebae711d3820c06
liveforone <yc4852@gmail.com>
2023-05-02 13:41:46 +0900

    Add param constant.

---
+++ timetable-service/timetable-service/src/main/java/middle/timetableservice/controller/constant/ParamConstant.java
@@ -0,0 +1,7 @@
+package middle.timetableservice.controller.constant;
+
+public final class ParamConstant {
+    public static final String SHOP_ID = "shopId";
+    public static final String LAST_ID = "lastId";
+    public static final String DEFAULT_ID = "0";
+}
