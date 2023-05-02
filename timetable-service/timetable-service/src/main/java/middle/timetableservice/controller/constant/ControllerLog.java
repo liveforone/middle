84d1eb293df3ebaf79558908a4d2aef708e6630e
liveforone <yc4852@gmail.com>
2023-05-02 16:39:44 +0900

    Add delete success controller log.

--- timetable-service/timetable-service/src/main/java/middle/timetableservice/controller/constant/ControllerLog.java
+++ timetable-service/timetable-service/src/main/java/middle/timetableservice/controller/constant/ControllerLog.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum ControllerLog {
 
     CREATE_TIMETABLE_SUCCESS("타임테이블을 성공적으로 등록했습니다."),
-    UPDATE_TIME_SUCCESS("시간을 성공적으로 업데이트 했습니다.");
+    UPDATE_TIME_SUCCESS("시간을 성공적으로 업데이트 했습니다."),
+    DELETE_TIMETABLE_SUCCESS("타임테이블을 성공적으로 삭제했습니다. | ID : ");
 
     private final String value;
 }
