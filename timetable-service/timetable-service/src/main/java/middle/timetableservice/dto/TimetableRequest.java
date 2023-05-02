264e99f8998a2350da59b0a56f0892bb6de82d8e
liveforone <yc4852@gmail.com>
2023-05-02 17:07:06 +0900

    Add builder in request dto for comfortable test.

--- timetable-service/timetable-service/src/main/java/middle/timetableservice/dto/TimetableRequest.java
+++ timetable-service/timetable-service/src/main/java/middle/timetableservice/dto/TimetableRequest.java
@@ -1,11 +1,15 @@
 package middle.timetableservice.dto;
 
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class TimetableRequest {
 
     @Positive(message = "예약 시간을 반드시 기입해주세요.")
@@ -14,5 +18,4 @@ public class TimetableRequest {
 
     @Positive(message = "예약 가능한 수는 반드시 기입해주세요.")
     private long basicRemaining;
-    private long remaining;
 }
