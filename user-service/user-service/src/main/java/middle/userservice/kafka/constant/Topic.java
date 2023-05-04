5f1f50649f99340f75a06324d9b0634837908518
liveforone <yc4852@gmail.com>
2023-05-04 10:59:51 +0900

    Refactor topic constant changed to static import.

--- user-service/user-service/src/main/java/middle/userservice/kafka/constant/Topic.java
+++ user-service/user-service/src/main/java/middle/userservice/kafka/constant/Topic.java
@@ -1,5 +1,6 @@
 package middle.userservice.kafka.constant;
 
 public final class Topic {
+    private Topic() {}
     public static final String REMOVE_SHOP_BELONG_MEMBER = "remove-shop-belong-member";
 }
