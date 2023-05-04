1fe8109c58e565ccb3429b50b4240800e9d69b8c
liveforone <yc4852@gmail.com>
2023-05-04 10:38:28 +0900

    Refactor param constant changed to static import.

--- timetable-service/timetable-service/src/main/java/middle/timetableservice/controller/constant/ParamConstant.java
+++ timetable-service/timetable-service/src/main/java/middle/timetableservice/controller/constant/ParamConstant.java
@@ -1,6 +1,9 @@
 package middle.timetableservice.controller.constant;
 
 public final class ParamConstant {
+
+    private ParamConstant() {}
+
     public static final String SHOP_ID = "shopId";
     public static final String LAST_ID = "lastId";
     public static final String DEFAULT_ID = "0";
