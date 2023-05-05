9d728e07c41cf2e538828258ecee143478de0a1b
liveforone <yc4852@gmail.com>
2023-05-05 17:21:16 +0900

    Add cancel reserve timetable url.

--- timetable-service/timetable-service/src/main/java/middle/timetableservice/provideController/constant/ProvideUrlToReservation.java
+++ timetable-service/timetable-service/src/main/java/middle/timetableservice/provideController/constant/ProvideUrlToReservation.java
@@ -3,5 +3,6 @@ package middle.timetableservice.provideController.constant;
 public final class ProvideUrlToReservation {
     private ProvideUrlToReservation() {}
 
-    public static final String MINUS_REMAINING = "/timetable/minus-remaining/{id}";
+    public static final String RESERVE_TIMETABLE = "/reserve/timetable/{id}";
+    public static final String CANCEL_RESERVE_TIMETABLE = "/cancel-reserve/timetable/{id}";
 }
