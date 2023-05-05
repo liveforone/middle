339701a7124a49b21563db9f71d5802fb305f3f5
liveforone <yc4852@gmail.com>
2023-05-05 18:00:01 +0900

    Refactor cancel reservation url.

--- timetable-service/timetable-service/src/main/java/middle/timetableservice/provideController/constant/ProvideUrlToReservation.java
+++ timetable-service/timetable-service/src/main/java/middle/timetableservice/provideController/constant/ProvideUrlToReservation.java
@@ -4,5 +4,5 @@ public final class ProvideUrlToReservation {
     private ProvideUrlToReservation() {}
 
     public static final String RESERVE_TIMETABLE = "/reserve/timetable/{id}";
-    public static final String CANCEL_RESERVE_TIMETABLE = "/cancel-reserve/timetable/{id}";
+    public static final String CANCEL_TIMETABLE = "/cancel/timetable/{id}";
 }
