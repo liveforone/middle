5f3b9995aa3c346187af49fd3888d4cadbeccbb9
liveforone <yc4852@gmail.com>
2023-05-05 16:20:55 +0900

    Initial reservation service.

---
+++ reservation-service/reservation-service/src/test/java/middle/reservationservice/ReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package middle.reservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
