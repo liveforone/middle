66bcbe2284c00c12e2546003c4c60b76fd1f531f
liveforone <yc4852@gmail.com>
2023-05-05 16:21:53 +0900

    Add binding custom exception.

---
+++ reservation-service/reservation-service/src/main/java/middle/reservationservice/exception/BindingCustomException.java
@@ -0,0 +1,8 @@
+package middle.reservationservice.exception;
+
+public class BindingCustomException extends RuntimeException{
+
+    public BindingCustomException(String message) {
+        super(message);
+    }
+}
