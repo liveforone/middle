fe0caf14a5dc87907dffa644b1fa826bc92c4854
liveforone <yc4852@gmail.com>
2023-05-05 16:33:15 +0900

    Add timetable url.

---
+++ reservation-service/reservation-service/src/main/java/middle/reservationservice/feignClient/constant/TimetableUrl.java
@@ -0,0 +1,8 @@
+package middle.reservationservice.feignClient.constant;
+
+public final class TimetableUrl {
+    private TimetableUrl() {}
+
+    public static final String BASE = "timetable-service";
+    public static final String RESERVE_TIMETABLE = "/reserve/timetable";
+}
