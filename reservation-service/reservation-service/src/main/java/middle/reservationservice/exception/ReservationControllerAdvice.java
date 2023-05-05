992f04d1ecb5765501d02fe38f1d75d79520d4e6
liveforone <yc4852@gmail.com>
2023-05-05 16:22:18 +0900

    Add reservation controller advice.

---
+++ reservation-service/reservation-service/src/main/java/middle/reservationservice/exception/ReservationControllerAdvice.java
@@ -0,0 +1,24 @@
+package middle.reservationservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ReservationControllerAdvice {
+
+    @ExceptionHandler(ReservationCustomException.class)
+    protected ResponseEntity<?> shopCustomHandle(ReservationCustomException customException) {
+        return ResponseEntity
+                .status(customException.getResponseMessage().getStatus())
+                .body(customException.getResponseMessage().getValue());
+    }
+
+    @ExceptionHandler(BindingCustomException.class)
+    protected ResponseEntity<?> bindingErrorHandle(BindingCustomException customException) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(customException.getMessage());
+    }
+}
