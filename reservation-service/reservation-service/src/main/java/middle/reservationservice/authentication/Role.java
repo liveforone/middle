3337e8393be29df519fa95cba1d05b61b0567752
liveforone <yc4852@gmail.com>
2023-05-05 16:21:36 +0900

    Add authentication info logic.

---
+++ reservation-service/reservation-service/src/main/java/middle/reservationservice/authentication/Role.java
@@ -0,0 +1,15 @@
+package middle.reservationservice.authentication;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Role {
+
+    ADMIN("ROLE_ADMIN"),
+    MEMBER("ROLE_MEMBER"),
+    OWNER("ROLE_OWNER");
+
+    private String value;
+}
