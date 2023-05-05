ce71fa52225ba67d03690494fdee553de3ae9333
liveforone <yc4852@gmail.com>
2023-05-05 18:25:00 +0900

    Add jpa auditing.

--- reservation-service/reservation-service/src/main/java/middle/reservationservice/ReservationServiceApplication.java
+++ reservation-service/reservation-service/src/main/java/middle/reservationservice/ReservationServiceApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableJpaAuditing
 public class ReservationServiceApplication {
 
 	public static void main(String[] args) {
