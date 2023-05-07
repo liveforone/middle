dd436fdcdd8cdb1895167b9ae8e479456d4a2cde
liveforone <yc4852@gmail.com>
2023-05-07 20:28:20 +0900

    Add my reservation url.

--- reservation-service/reservation-service/src/main/java/middle/reservationservice/controller/constant/ReservationUrl.java
+++ reservation-service/reservation-service/src/main/java/middle/reservationservice/controller/constant/ReservationUrl.java
@@ -4,4 +4,5 @@ public final class ReservationUrl {
     private ReservationUrl() {}
 
     public static final String RESERVATION_DETAIL = "/reservation/detail/{id}";
+    public static final String MY_RESERVATION = "/reservation/me";
 }
