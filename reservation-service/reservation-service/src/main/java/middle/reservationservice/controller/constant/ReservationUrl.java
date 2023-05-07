d6f97e3787b93e67ccd9df0dfd8e77a1b405a539
liveforone <yc4852@gmail.com>
2023-05-07 21:08:40 +0900

    Add reservation belong to shop url.

--- reservation-service/reservation-service/src/main/java/middle/reservationservice/controller/constant/ReservationUrl.java
+++ reservation-service/reservation-service/src/main/java/middle/reservationservice/controller/constant/ReservationUrl.java
@@ -5,4 +5,5 @@ public final class ReservationUrl {
 
     public static final String RESERVATION_DETAIL = "/reservation/detail/{id}";
     public static final String MY_RESERVATION = "/reservation/me";
+    public static final String RESERVATION_BELONG_SHOP = "/reservation/shop/{shopId}";
 }
