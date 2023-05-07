1415358045db52b54dbb9fefa08be402d1cd9d65
liveforone <yc4852@gmail.com>
2023-05-07 20:06:24 +0900

    Add id param in reservation param.

---
+++ reservation-service/reservation-service/src/main/java/middle/reservationservice/controller/constant/ReservationParam.java
@@ -0,0 +1,9 @@
+package middle.reservationservice.controller.constant;
+
+public final class ReservationParam {
+    private ReservationParam() {
+    }
+
+    public static final String ID = "id";
+
+}
