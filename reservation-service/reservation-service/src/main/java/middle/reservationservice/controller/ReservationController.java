bc0206555d57ad10db95cd0f9a7309b5e087128d
liveforone <yc4852@gmail.com>
2023-05-07 21:01:46 +0900

    Add my reservation controller method.

--- reservation-service/reservation-service/src/main/java/middle/reservationservice/controller/ReservationController.java
+++ reservation-service/reservation-service/src/main/java/middle/reservationservice/controller/ReservationController.java
@@ -11,8 +11,12 @@ import middle.reservationservice.validator.ReservationValidator;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+import static middle.reservationservice.controller.constant.ReservationParam.*;
 import static middle.reservationservice.controller.constant.ReservationUrl.*;
 
 @RestController
@@ -34,5 +38,15 @@ public class ReservationController {
 
         ReservationResponse reservation = reservationService.getReservationById(id);
         return ResponseEntity.ok(reservation);
+    }
+
+    @GetMapping(MY_RESERVATION)
+    public ResponseEntity<List<ReservationResponse>> myReservation(
+            @RequestParam(name = LAST_ID) Long lastId,
+            HttpServletRequest request
+    ) {
+        List<ReservationResponse> reservations = reservationService
+                .getReservationsByUsername(authenticationInfo.getUsername(request), lastId);
+        return ResponseEntity.ok(reservations);
     }
 }
