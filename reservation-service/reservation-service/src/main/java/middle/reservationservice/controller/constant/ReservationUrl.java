5bac943a7fe5611b2068bd70242ac58197ecb008
liveforone <yc4852@gmail.com>
2023-05-07 20:06:34 +0900

    Add reservation detail url.

---
+++ reservation-service/reservation-service/src/main/java/middle/reservationservice/controller/constant/ReservationUrl.java
@@ -0,0 +1,7 @@
+package middle.reservationservice.controller.constant;
+
+public final class ReservationUrl {
+    private ReservationUrl() {}
+
+    public static final String RESERVATION_DETAIL = "/reservation/detail/{id}";
+}
