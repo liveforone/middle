e6ac9eae056de96f8e7c810fb15d03d4b5242edb
liveforone <yc4852@gmail.com>
2023-05-07 20:05:23 +0900

    Add reservation is null response message.

--- reservation-service/reservation-service/src/main/java/middle/reservationservice/controller/restResponse/ResponseMessage.java
+++ reservation-service/reservation-service/src/main/java/middle/reservationservice/controller/restResponse/ResponseMessage.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ResponseMessage {
 
+    RESERVATION_IS_NULL(404, "예약이 존재하지 않습니다."),
+
     RESERVE_SUCCESS(201, "예약에 성공하였습니다.");
 
     private final int status;
