bedafd7c47ecc01c63203118ec88f476af63f283
liveforone <yc4852@gmail.com>
2023-05-07 19:25:06 +0900

    Add get shop id feign url.

--- reservation-service/reservation-service/src/main/java/middle/reservationservice/feignClient/constant/TimetableUrl.java
+++ reservation-service/reservation-service/src/main/java/middle/reservationservice/feignClient/constant/TimetableUrl.java
@@ -4,6 +4,7 @@ public final class TimetableUrl {
     private TimetableUrl() {}
 
     public static final String BASE = "timetable-service";
+    public static final String GET_SHOP_ID = "/get/shop-id/{id}";
     public static final String RESERVE_TIMETABLE = "/reserve/timetable/{id}";
     public static final String CANCEL_TIMETABLE = "/cancel/timetable/{id}";
 }
