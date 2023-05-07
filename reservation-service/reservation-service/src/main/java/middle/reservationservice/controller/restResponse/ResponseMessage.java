4ee39a57a00bb344e47f96aaf1d84e1748154a2f
liveforone <yc4852@gmail.com>
2023-05-07 23:03:36 +0900

    Add cancel success response message.

--- reservation-service/reservation-service/src/main/java/middle/reservationservice/controller/restResponse/ResponseMessage.java
+++ reservation-service/reservation-service/src/main/java/middle/reservationservice/controller/restResponse/ResponseMessage.java
@@ -12,7 +12,8 @@ public enum ResponseMessage {
     RESERVE_FAIL(400, "잔여 정원을 초과되어 예약을 할 수 없습니다."),
     CANT_FIND_SHOP(404, "상점을 찾을 수 없습니다."),
     RESERVE_SUCCESS(201, "예약에 성공하였습니다."),
-    CANCEL_FAIL(400, "예약 취소가 불가능한 날짜입니다.");
+    CANCEL_FAIL(400, "예약 취소가 불가능한 날짜입니다."),
+    CANCEL_SUCCESS(200, "예약을 성공적으로 취소하였습니다.");
 
     private final int status;
     private final String value;
