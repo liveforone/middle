213aa3b5651ad2ad6bf08bdbe7ea75686ebc6e39
liveforone <yc4852@gmail.com>
2023-05-07 18:18:26 +0900

    Add find one by id query method.

--- reservation-service/reservation-service/src/main/java/middle/reservationservice/repository/ReservationCustomRepository.java
+++ reservation-service/reservation-service/src/main/java/middle/reservationservice/repository/ReservationCustomRepository.java
@@ -1,4 +1,7 @@
 package middle.reservationservice.repository;
 
+import middle.reservationservice.domain.Reservation;
+
 public interface ReservationCustomRepository {
+    Reservation findOneById(Long id);
 }
--- reservation-service/reservation-service/src/main/java/middle/reservationservice/repository/ReservationRepositoryImpl.java
+++ reservation-service/reservation-service/src/main/java/middle/reservationservice/repository/ReservationRepositoryImpl.java
@@ -2,11 +2,21 @@ package middle.reservationservice.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import middle.reservationservice.domain.QReservation;
+import middle.reservationservice.domain.Reservation;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
-public class ReservationRepositoryImpl {
+public class ReservationRepositoryImpl implements ReservationCustomRepository {
 
     private final JPAQueryFactory queryFactory;
+    QReservation reservation = QReservation.reservation;
+
+    public Reservation findOneById(Long id) {
+        return queryFactory
+                .selectFrom(reservation)
+                .where(reservation.id.eq(id))
+                .fetchOne();
+    }
 }
