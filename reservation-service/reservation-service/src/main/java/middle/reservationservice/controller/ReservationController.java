5eb05cbe4f76880d5b84ac797b3d2f2ced0d64ef
liveforone <yc4852@gmail.com>
2023-05-07 21:55:47 +0900

    Add reserve controller method.

--- reservation-service/reservation-service/src/main/java/middle/reservationservice/controller/ReservationController.java
+++ reservation-service/reservation-service/src/main/java/middle/reservationservice/controller/ReservationController.java
@@ -4,14 +4,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import middle.reservationservice.authentication.AuthenticationInfo;
+import middle.reservationservice.controller.constant.ControllerLog;
+import middle.reservationservice.controller.restResponse.RestResponse;
 import middle.reservationservice.dto.ReservationResponse;
+import middle.reservationservice.feignClient.TimetableFeignService;
+import middle.reservationservice.feignClient.constant.CircuitLog;
 import middle.reservationservice.service.ReservationService;
 import middle.reservationservice.validator.ReservationValidator;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +28,8 @@ public class ReservationController {
     private final ReservationService reservationService;
     private final AuthenticationInfo authenticationInfo;
     private final ReservationValidator reservationValidator;
+    private final TimetableFeignService timetableFeignService;
+    private final CircuitBreakerFactory<?, ?> circuitBreakerFactory;
 
     @GetMapping(RESERVATION_DETAIL)
     public ResponseEntity<?> reservationDetail(
@@ -56,5 +60,43 @@ public class ReservationController {
     ) {
         List<ReservationResponse> reservations = reservationService.getReservationsByShopId(shopId, lastId);
         return ResponseEntity.ok(reservations);
+    }
+
+    @PostMapping(RESERVE)
+    public ResponseEntity<?> reserve(
+            @PathVariable(TIMETABLE_ID) Long timetableId,
+            HttpServletRequest request
+    ) {
+        reservationValidator.validateReserveTimetable(reserveTimetable(timetableId));
+
+        Long shopId = getShopId(timetableId);
+        reservationValidator.validateShop(shopId);
+
+        reservationService.reserve(
+                timetableId,
+                authenticationInfo.getUsername(request),
+                shopId
+        );
+        log.info(ControllerLog.RESERVE_SUCCESS.getValue());
+
+        return RestResponse.reserveSuccess();
+    }
+
+    private boolean reserveTimetable(Long timetableId) {
+        return circuitBreakerFactory
+                .create(CircuitLog.TIMETABLE_CIRCUIT_LOG.getValue())
+                .run(
+                        ()-> timetableFeignService.reserveTimetable(timetableId),
+                        throwable -> false
+                );
+    }
+
+    private Long getShopId(Long timetableId) {
+        return circuitBreakerFactory
+                .create(CircuitLog.TIMETABLE_CIRCUIT_LOG.getValue())
+                .run(
+                        ()-> timetableFeignService.getShopId(timetableId),
+                        throwable -> null
+                );
     }
 }
