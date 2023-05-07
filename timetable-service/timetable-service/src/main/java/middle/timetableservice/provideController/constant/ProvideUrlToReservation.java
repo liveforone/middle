84e32beb3af8f8279a2f9641212d0e896a71411a
liveforone <yc4852@gmail.com>
2023-05-07 19:21:41 +0900

    Add get shop id provide url.

--- timetable-service/timetable-service/src/main/java/middle/timetableservice/provideController/constant/ProvideUrlToReservation.java
+++ timetable-service/timetable-service/src/main/java/middle/timetableservice/provideController/constant/ProvideUrlToReservation.java
@@ -3,6 +3,7 @@ package middle.timetableservice.provideController.constant;
 public final class ProvideUrlToReservation {
     private ProvideUrlToReservation() {}
 
+    public static final String GET_SHOP_ID = "/get/shop-id/{id}";
     public static final String RESERVE_TIMETABLE = "/reserve/timetable/{id}";
     public static final String CANCEL_TIMETABLE = "/cancel/timetable/{id}";
 }
