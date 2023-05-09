1e63ce4990d419747c5e0c51f5aa66b85fbb9f9b
liveforone <yc4852@gmail.com>
2023-05-09 11:40:41 +0900

    Rename shop is good and bad topic.

--- shop-service/shop-service/src/main/java/middle/shopservice/kafka/constant/Topic.java
+++ shop-service/shop-service/src/main/java/middle/shopservice/kafka/constant/Topic.java
@@ -4,10 +4,7 @@ public final class Topic {
 
     private Topic() {}
 
-    public static final String SHOP_IS_GOOD = "shop-is-good";
-    public static final String SHOP_IS_BAD = "shop-is-bad";
     public static final String REMOVE_SHOP_BELONG_MEMBER = "remove-shop-belong-member";
     public static final String REMOVE_RECOMMEND = "remove-recommend";
     public static final String REMOVE_TIMETABLE = "remove-timetable";
-    public static final String REMOVE_REVIEW = "remove-review";
 }
