90aff845884b028ce85643550028f55c98aa659f
liveforone <yc4852@gmail.com>
2023-05-09 10:45:09 +0900

    Add async logic.

---
+++ review-service/review-service/src/main/java/middle/reviewservice/async/AsyncConfig.java
@@ -0,0 +1,24 @@
+package middle.reviewservice.async;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(AsyncConstant.commandAsync)
+    public Executor commandAsync() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(AsyncConstant.CORE_POOL_SIZE);
+        executor.setMaxPoolSize(AsyncConstant.MAX_POOL_SIZE);
+        executor.setQueueCapacity(AsyncConstant.QUEUE_CAPACITY);
+        executor.setThreadNamePrefix(AsyncConstant.commandAsync);
+        executor.initialize();
+        return executor;
+    }
+}
---
+++ review-service/review-service/src/main/java/middle/reviewservice/async/AsyncConstant.java
@@ -0,0 +1,10 @@
+package middle.reviewservice.async;
+
+public final class AsyncConstant {
+    private AsyncConstant() {}
+
+    public static final String commandAsync = "commandAsync";
+    public static final int CORE_POOL_SIZE = 10;
+    public static final int MAX_POOL_SIZE = 20;
+    public static final int QUEUE_CAPACITY = 100;
+}
