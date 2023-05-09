a8326ab47243bbd7d524980c0248f491e22a6ee5
liveforone <yc4852@gmail.com>
2023-05-09 10:48:51 +0900

    Add binding custom runtime exception class.

---
+++ review-service/review-service/src/main/java/middle/reviewservice/exception/BindingCustomException.java
@@ -0,0 +1,8 @@
+package middle.reviewservice.exception;
+
+public class BindingCustomException extends RuntimeException{
+
+    public BindingCustomException(String message) {
+        super(message);
+    }
+}
