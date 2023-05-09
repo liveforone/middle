14a3afa51eaaf4a283996189e85c294186141752
liveforone <yc4852@gmail.com>
2023-05-09 11:33:08 +0900

    Remove review service.

--- review-service/review-service/src/main/java/middle/reviewservice/kafka/constant/KafkaServer.java
+++
@@ -1,9 +0,0 @@
-package middle.reviewservice.kafka.constant;
-
-public final class KafkaServer {
-
-    private KafkaServer() {}
-
-    public static final String KAFKA_SERVER_POST = "localhost:9092";
-//    public static final String KAFKA_SERVER_POST = "172.18.0.101:9092";
-}
