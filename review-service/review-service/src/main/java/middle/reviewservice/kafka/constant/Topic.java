aa5e206d99d6dca9a36429e7b799ad21ef4be837
liveforone <yc4852@gmail.com>
2023-05-09 11:00:40 +0900

    Add kafka topic constant.

---
+++ review-service/review-service/src/main/java/middle/reviewservice/kafka/constant/Topic.java
@@ -0,0 +1,10 @@
+package middle.reviewservice.kafka.constant;
+
+public final class Topic {
+
+    private Topic() {}
+
+    public static final String SHOP_IS_GOOD = "shop-is-good";
+    public static final String SHOP_IS_BAD = "shop-is-bad";
+    public static final String REMOVE_REVIEW = "remove-review";
+}
