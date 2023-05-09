28255b8129ddda2a66d43f69ea6f541ca393f408
liveforone <yc4852@gmail.com>
2023-05-09 10:43:53 +0900

    Initial review-service.

---
+++ review-service/review-service/src/main/java/middle/reviewservice/ReviewServiceApplication.java
@@ -0,0 +1,29 @@
+package middle.reviewservice;
+
+import feign.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class ReviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewServiceApplication.class, args);
+	}
+
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
+	}
+
+	@Bean
+	public Logger.Level feignLoggerLeve() {
+		return Logger.Level.FULL;
+	}
+}
---
+++ review-service/review-service/src/test/java/middle/reviewservice/ReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package middle.reviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
